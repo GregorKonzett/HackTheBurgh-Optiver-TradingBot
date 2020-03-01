41d3bcc1467ec16fb0c22febd2062c90ab048468
Reason1338 <gregor.konzett98@gmail.com>
2020-03-01 12:00:43 +0000

    Machine Learning Pls Fix

--- SimpleTrader.py
+++ SimpleTrader.py
@@ -29,7 +29,7 @@ def bestLine(prices):
 	return a,b
 
 
-pod = 50
+pod = 10
 # clf = linear_model.LinearRegression()
 clf = ensemble.GradientBoostingRegressor(n_estimators = 500, max_depth = 10, min_samples_split = 2,
 	learning_rate = 0.2, loss = 'ls')
@@ -49,6 +49,17 @@ pred_bid_ESX = -1
 pred_ask_SP = -1
 pred_bid_SP = -1
 
+esx_last_bought = -1
+sp_last_bought = -1
+
+esx_last_bid = -1
+sp_last_bid = -1
+
+esx_bidPrice = 0
+esx_askPrice = 0
+
+sp_bidPrice = 0
+sp_askPrice = 0
 
 def gradientBoosting(pair, old_list, trade_list, biddi): 
 	if len(old_list) < pod+1 or len(trade_list) < pod: 
@@ -73,7 +84,8 @@ def gradientBoosting(pair, old_list, trade_list, biddi):
 		return y_pred
 
 
-THRESHHOLD = 0.2
+THRESHHOLD = 0.24
+AMOUNT = 0.3
 informationRetriever = InformationRetriever("35.179.45.135",7001)
 sender = Sender()
 
@@ -85,21 +97,22 @@ while True:
 		instrument = 'ESX-FUTURE'
 		price, volume = float(data.split("|")[3].split("=")[1]),float(data.split("|")[4].split("=")[1])
 		pred_bid_ESX = gradientBoosting([price, volume], old_hist_ESX_Bid, trade_hist_ESX_Bid, True)
+		esx_last_bid = esx_bidPrice
 
-		if (pred_bid_ESX > 0 and pred_bid_ESX + THRESHHOLD < bidPrice):
+		if (pred_bid_ESX > 0 and pred_bid_ESX + THRESHHOLD < esx_bidPrice and esx_bidPrice > esx_last_bought):
 			#Sell
-			print("Selling 15 ESX at ",bidPrice)
-			sender.send_order(instrument,"SELL",bidPrice,15)
+			print("Selling 50 ESX at ",esx_bidPrice)
+			sender.send_order(instrument,"SELL",esx_bidPrice,round(volume*AMOUNT))
 
- 
- 	elif("TYPE=TRADE|FEEDCODE=ESX-FUTURE|SIDE=ASK" in data):
+	elif("TYPE=TRADE|FEEDCODE=ESX-FUTURE|SIDE=ASK" in data):
 		instrument = 'ESX-FUTURE'
 		price, volume = float(data.split("|")[3].split("=")[1]),float(data.split("|")[4].split("=")[1])
 		pred_ask_ESX = gradientBoosting([price, volume], old_hist_ESX_Ask, trade_hist_ESX_Ask, False)
-		if (pred_ask_ESX > 0 and pred_ask_ESX - THRESHHOLD >= askPrice):
+		if (pred_ask_ESX > 0 and pred_ask_ESX - THRESHHOLD >= esx_askPrice and esx_askPrice < esx_last_bid):
 			#Buy
-			print("Buying 15 ESX at ",askPrice)
-			sender.send_order(instrument,"BUY",askPrice,15)
+			esx_last_bought = esx_askPrice
+			print("Buying 50 ESX at ",esx_askPrice)
+			sender.send_order(instrument,"BUY",esx_askPrice,round(volume*AMOUNT))
 
 
 	elif("TYPE=TRADE|FEEDCODE=SP-FUTURE|SIDE=BID" in data):
@@ -107,36 +120,38 @@ while True:
 		price, volume = float(data.split("|")[3].split("=")[1]),float(data.split("|")[4].split("=")[1])
 		pred_bid_SP = gradientBoosting([price, volume], old_hist_SP_Bid, trade_hist_SP_Bid, True)
 
-		if (pred_bid_SP > 0 and pred_bid_SP + THRESHHOLD < bidPrice):
+		sp_last_bid = sp_bidPrice
+		if (pred_bid_SP > 0 and pred_bid_SP + THRESHHOLD < sp_bidPrice and sp_bidPrice > sp_last_bought):
 			#print(bidPrice,(a+(len(bidPrices)+1)*b))
 			#Sell
-			print("Selling 15 SP at ",bidPrice)
-			sender.send_order(instrument,"SELL",bidPrice,15)
+			print("Selling 50 SP at ",sp_bidPrice)
+			sender.send_order(instrument,"SELL",sp_bidPrice,round(volume*AMOUNT))
 
  
- 	elif("TYPE=TRADE|FEEDCODE=ESX-FUTURE|SIDE=ASK" in data):
+	elif("TYPE=TRADE|FEEDCODE=SP-FUTURE|SIDE=ASK" in data):
 		instrument = 'SP-FUTURE'
 		price, volume = float(data.split("|")[3].split("=")[1]),float(data.split("|")[4].split("=")[1])
 		pred_ask_SP = gradientBoosting([price, volume], old_hist_SP_Ask, trade_hist_SP_Ask, False)
 
-		if (pred_ask_SP > 0 and pred_ask_SP - THRESHHOLD > askPrice):
+		if (pred_ask_SP > 0 and pred_ask_SP - THRESHHOLD > sp_askPrice and sp_askPrice < sp_last_bid):
 			#Buy
-			print("Buying 15 SP at ",askPrice)
-			sender.send_order(instrument,"BUY",askPrice,15)
+			sp_last_bought = sp_askPrice
+			print("Buying 50 SP at ",sp_askPrice)
+			sender.send_order(instrument,"BUY",sp_askPrice,round(volume*AMOUNT))
 
 
 	elif("TYPE=PRICE|FEEDCODE=SP-FUTURE" in data):
 		instrument = 'SP-FUTURE'
-		bidPrice, askPrice = getPrices(data)
-		old_hist_SP_Bid.append(bidPrice)
-		old_hist_SP_Ask.append(askPrice)
+		sp_bidPrice, sp_askPrice = getPrices(data)
+		old_hist_SP_Bid.append(sp_bidPrice)
+		old_hist_SP_Ask.append(sp_askPrice)
 
 
 	elif("TYPE=PRICE|FEEDCODE=ESX-FUTURE" in data):
 		instrument = 'ESX-FUTURE'
-		bidPrice, askPrice = getPrices(data)
-		old_hist_ESX_Bid.append(bidPrice)
-		old_hist_ESX_Ask.append(askPrice)
+		esx_bidPrice, esx_askPrice = getPrices(data)
+		old_hist_ESX_Bid.append(esx_bidPrice)
+		old_hist_ESX_Ask.append(esx_askPrice)
 
 
 
